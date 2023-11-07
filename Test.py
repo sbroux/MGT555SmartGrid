5a58c33dbdfa66f28e07312ead27cd77d9a11d02
Lina Bousbina <lina.bousbina@epfl.ch>
2023-11-07 20:24:49 +0100

    mm

--- Test.py
+++
@@ -1,3 +0,0 @@
-# Decision Making Algorithm For Testing
-
-import cnt from controller
---
+++ blink.py
@@ -0,0 +1,27 @@
+# Decision Making Algorithm For Testing
+
+import serial
+import time
+import pyfirmata
+
+comport = "COM3"  # change with the port u are using
+
+board = pyfirmata.Arduino(comport)
+
+# Reçoit en input un tableau binaire,
+# 0 ou 1 pour dire si une led strip/ indicateur Leds est allumé ou pas. Output, illumine les Leds.
+
+led_1 = board.get_pin("d:8:o")  # on choisis le pin
+
+
+def led(light):
+    if light == [0]:
+        led_1.write(1)
+
+    elif light == [1]:
+        led_1.write(0)
+
+
+while True:
+    light = led_1.read()
+    led(light)
