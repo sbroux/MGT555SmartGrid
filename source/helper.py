64a9ad826ce2049b5bd0811b84ddae475e11fb1f
Lina Bousbina <lina.bousbina@epfl.ch>
2023-12-04 11:02:24 +0100

    jj

--- source/helper.py
+++ source/helper.py
@@ -2,6 +2,7 @@ from datetime import datetime, timedelta
 import serial
 import time
 import pandas as pd
+import numpy as np
 
 time_scaling_factor = 3600 / 10
 start_time = pd.Timestamp("2023-12-01 00:00:00")
@@ -83,29 +84,32 @@ def charge_vehicle_with_chargers(
     return : energy trajectory"""
 
     if stockage_room_level >= 100 / 9:
-        yield current_time.timeout(charging_time)
+        # yield current_time.timeout(1)
         stockage_room_level -= 100 / 9
         print(
             "charing with chargers + room2 , current room2 level: ", stockage_room_level
         )
-        return room2_chargers
+        return room2_chargers, stockage_room_level, energy_cost
     else:
-        yield current_time.timeout(charging_time)
+        # yield current_time.timeout(charging_time)
         energy_cost += energy_price_grid * vehicle_battery_capacity / 100
         print("charing with chargers + grid , current energy cost: ", energy_cost)
-        return grid_chargers
+        return grid_chargers, stockage_room_level, energy_cost
 
 
 def charge_stockage_room_with_solar_pannel(
-    current_time, solar_pannel_power, stockage_room_level
+    current_time, solar_pannel_power, stockage_room_level, charging_time
 ):
     """
-    We charge the stockage room with the solar pannel
+    We charge the stockage room with the solar pannel ,
+      the stockage room level is incremented depending on the charging time
     """
     # yield current_time.timeout(1)
-    stockage_room_level += solar_pannel_power
+    max = 500 * 9
+    while stockage_room_level < max:
+        stockage_room_level += solar_pannel_power * charging_time / 3600
     print("charging with solar pannel, current room2 level: ", stockage_room_level)
-    return stockage_room_level
+    return solar_room2, stockage_room_level
 
 
 def charge_stockage_room_with_grid(
@@ -116,10 +120,61 @@ def charge_stockage_room_with_grid(
     energy cost is incremented
     """
     # yield current_time.timeout(1)
-
-    stockage_room_level += 100 / 9
+    max = 500 * 9
+    while stockage_room_level < max:
+        if energy_price_grid < 15:
+            energy_cost += energy_price_grid * 100 / 9
+            stockage_room_level += 100 / 9
     print("charging with grid, current room2 level: ", stockage_room_level)
-    return stockage_room_level
+    return grid_room2, stockage_room_level, energy_cost
+
+
+def charge_swapping_with_stockage(
+    env, number_of_battery_charged, swapping_room_slots, stockage_room_level
+):
+    """
+    We charge the swapping room with the stockage room
+    return : trajectory, swapping_room_slots, number_of_battery_charged, stockage_room_level
+    """
+    # yield env.timeout(1)
+    while stockage_room_level > 0:
+        if number_of_battery_charged < 9:
+            number_of_battery_charged += 1
+            swapping_room_slots[number_of_battery_charged] = 1
+            stockage_room_level -= 100 / 9
+    print(
+        "charging swapping room with stockage room, current swapping room level: ",
+        swapping_room_slots,
+    )
+    return (
+        room2_room1,
+        stockage_room_level,
+        number_of_battery_charged,
+        swapping_room_slots,
+    )
+
+
+def charge_swapping_with_grid(
+    env,
+    number_of_battery_charged,
+    swapping_room_slots,
+    energy_price_grid,
+    stockage_room_level,
+    energy_cost,
+):
+    """
+    We charge the swapping room with the grid"""
+
+    # yield env.timeout(1)
+    while number_of_battery_charged < 9:
+        energy_cost += energy_price_grid * 100 / 9
+        number_of_battery_charged += 1
+        swapping_room_slots[number_of_battery_charged] = 1
+    print(
+        "charging swapping room with grid, current swapping room level: ",
+        swapping_room_slots,
+    )
+    return grid_room1, stockage_room_level, number_of_battery_charged, energy_cost
 
 
 def decision_making(
@@ -135,13 +190,16 @@ def decision_making(
     solar_pannel_power,
 ):
     # Define initial battery levels and constraints
-
+    # prendre la valeur dnans le data frame au temps current et check si ya solar energy
     while env.now:
-        threshold = 10
+        threshold = 100 / 9
+        threshold_pannel_power = 100
+        charging_time = vehicle_battery_capacity / 150
+        #
 
         if solar_pannel_power > threshold:
             stockage_room_level = charge_stockage_room_with_solar_pannel(
-                env, solar_pannel_power
+                env, solar_pannel_power, stockage_room_level, charging_time * 9
             )
         else:
             stockage_room_level, energy_cost = charge_stockage_room_with_grid(
@@ -150,18 +208,25 @@ def decision_making(
 
         if stockage_room_level > threshold:
             (
+                stockage_room_level,
                 number_of_battery_charged,
                 swapping_room_slots,
             ) = charge_swapping_with_stockage(
                 env, number_of_battery_charged, swapping_room_slots, stockage_room_level
             )
+
         else:
-            number_of_battery_charged, swapping_room_slots = charge_swapping_with_grid(
+            (
+                number_of_battery_charged,
+                swapping_room_slots,
+                energy_cost,
+            ) = charge_swapping_with_grid(
                 env,
                 number_of_battery_charged,
                 swapping_room_slots,
                 energy_price_grid,
                 stockage_room_level,
+                energy_cost,
             )
 
         sim_time_datetime = datetime.utcfromtimestamp(env.now)
@@ -170,7 +235,6 @@ def decision_making(
             for index, row in vehicle_arrival_data.iterrows():
                 vehicle_id = row["Vehicle_ID"]
                 arrival_time = row["Date Time"]
-
                 arrival_time = datetime.strptime(arrival_time, "%Y-%m-%d %H:%M:%S")
                 # print("sim_time_datetime", sim_time_datetime)
                 if arrival_time == sim_time_datetime:
